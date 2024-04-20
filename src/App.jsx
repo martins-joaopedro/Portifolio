8753ccb34c80b3ad7688d395f93cf759d482a228
João Pedro Martins Cruz <103667165+martins-joaopedro@users.noreply.github.com>
2024-04-19 22:53:07 -0300

    feat: setup app

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,15 @@
+import { MainLayout } from "./components/MainLayout"
+import { About } from "./pages/About"
+import { Contact } from "./pages/Contact"
 import { Home } from "./pages/Home"
 
 function App() {
   return (
-    <>
+    <MainLayout>
       <Home />
-    </>  
+      <About />
+      <Contact />
+    </MainLayout>  
   )
 }
 
