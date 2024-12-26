6366fbfad87eacc8ba562545006aa7b1043ad498
João Pedro Martins <103667165+martins-joaopedro@users.noreply.github.com>
2024-12-26 10:04:17 -0300

    feat: tech cards

--- src/pages/Tech/index.jsx
+++ src/pages/Tech/index.jsx
@@ -1,6 +1,14 @@
 import { IconBase } from 'react-icons';
 import styles from './styles.module.css'
+
 import { RiJavascriptFill } from "react-icons/ri";
+import { FaJava } from "react-icons/fa";
+import { SiSpring } from "react-icons/si";
+import { FaHtml5 } from "react-icons/fa";
+import { SiCplusplus } from "react-icons/si";
+import { FaReact } from "react-icons/fa";
+import { MdOutlineCss } from "react-icons/md";
+import { IoLogoCss3 } from "react-icons/io";
 
 export const Tech = () => {
 
@@ -8,17 +16,42 @@ export const Tech = () => {
         {
             name: "Javascript",
             icon: <RiJavascriptFill />,
-        }
+        },
+        {
+            name: "HTML",
+            icon: <FaHtml5 />,
+        },
+        {
+            name: "CSS",
+            icon: <IoLogoCss3 />,
+        },
+        {
+            name: "React",
+            icon: <FaReact />,
+        },
+        {
+            name: "Java",
+            icon: <FaJava />,
+        },
+        {
+            name: "SpringBoot",
+            icon: <SiSpring />,
+        },
+        {
+            name: "C++",
+            icon: <SiCplusplus />,
+        },
     ]
 
   return (
-    <div>
+    <div className={styles.container}>
         { data.map(({name, icon}, i) => (
             <div 
                 key={i}
+                className={styles.card}
             >
-                {name}
                 {icon}
+                {name}
             </div>
         ))}
     </div>
