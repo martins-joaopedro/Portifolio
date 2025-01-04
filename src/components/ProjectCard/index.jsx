6548284c6fedced91d40c07afa758f80c33e876e
João Pedro Martins <103667165+martins-joaopedro@users.noreply.github.com>
2025-01-03 22:57:52 -0300

    feat: project cards

--- src/components/ProjectCard/index.jsx
+++ src/components/ProjectCard/index.jsx
@@ -1,20 +1,48 @@
+import { useEffect, useRef, useState } from 'react';
 import styles from './styles.module.css'
 
-export const ProjectCard = ({ name, image, description }) => {
+import { FaGit, FaGithub } from "react-icons/fa";
+import { FaRegWindowRestore } from "react-icons/fa";
+import { FaWindowMaximize } from 'react-icons/fa6';
+
+export const ProjectCard = ({ data }) => {
+
+  const { name, image, description, la, tech, results, date, status, colors } = data;
+
+  const STATUS = ["Concluído", "Em Andamento", "Pausado"]
+
   return (
-    <div className={styles.container}>
-        <div>
-            <img src={image} alt="" />
+    <div className={styles.view}>
+      <div className={styles.container}
+        style={{backgroundPosition: "70%", backgroundSize: "300%", background: `linear-gradient(-135deg, var(--bg-dark), ${colors[0]})` }}
+      >
+        <div className={styles.image}> 
+          <img src={image} alt="" />
         </div>
-        <div className={styles.projectInfo}>
-            <h1>{name}</h1>
-            <p>{description}</p>
-            <div className={styles.tech}>
-                <span>324</span>
-                <span>324</span>
-                <span>324</span>
-            </div>
-        </div>  
+        <div className={styles.details}>
+          <h3>{name}</h3>
+          <p className={styles.description}>{description}</p>
+          <span
+            status={status.toString()}
+            className={styles.status}
+          >
+            {STATUS[status]}
+          </span>
+          <div className={styles.tech}>
+            {
+              tech?.map(({ value }, key) => (
+                <span key={key}>{value}</span>
+              ))
+            }
+          </div>
+          <p>{results}</p>
+
+          <div className={styles.buttons}>
+            <button><FaGithub/></button>
+            <button><FaWindowMaximize /></button>
+          </div>
+        </div>
+      </div>
     </div>
   )
 }
--- src/pages/Projects/index.jsx
+++ src/pages/Projects/index.jsx
@@ -1,18 +1,15 @@
 import styles from './styles.module.css'
 import { ProjectCard } from '../../components/ProjectCard'
-
 import { data } from '../../data/projects.json'
 
 export const Projects = () => {
   return (
-    <div className={styles.container}>
+    <div className={styles.container} id="projects">
         <h1>projetos</h1>
         { data.map((project, key) => (
             <ProjectCard 
                 key={key}
-                name={project.name}
-                description={project.description}
-                image={project.image}
+                data={project}
             />
         ))}
     </div>
